7a21aee40c58e257ba4f9228e2cb78b9c2b59c15
Till Grosch <mail@tillgrosch.com>
2020-04-21 01:20:47 +0200

    heroku setup

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 8080;
 const server = require('http').Server(app);
 const db = require('./db');
 const io = require('socket.io')(server, {
-    origins: 'localhost:8080 192.168.0.123:8080',
+    origins:
+        'localhost:8080 192.168.0.123:8080 social-reaction.herokuapp.com:*',
 });
 
 const { SESSION_SECRET: sessionSecret } = process.env.SESSION_SECRET
